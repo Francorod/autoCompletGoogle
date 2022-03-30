08b90f4c67ff75d58ac07a715aebd32214fd03bf
Franco Rod <francorodios@gmail.com>
2022-03-30 14:13:46 -0400

    coding

--- autoCompletGoogle/ViewController.swift
+++ autoCompletGoogle/ViewController.swift
@@ -6,13 +6,31 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchResultsUpdating {
+
+    
+    let mapView = MKMapView()
+    
+    let searchVC = UISearchController(searchResultsController: ResultsViewController())
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Maps"
+        view.addSubview(mapView)
+        searchVC.searchBar.backgroundColor = .secondarySystemBackground
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
     }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        mapView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.size.width, height: view.frame.size.height - view.safeAreaInsets.top)
+    }
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
 
 
 }
